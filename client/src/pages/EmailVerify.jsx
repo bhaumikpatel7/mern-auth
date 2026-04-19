684642008abecf54340bc9c5cc9c9b56880c3715
bhaumikpatel7 <72443943+bhaumikpatel7@users.noreply.github.com>
2026-04-19 16:08:34 -0400

    frontend started

---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+
+
+const EmailVerify = () => {
+  return (
+    <div>E</div>
+  )
+}
+
+export default EmailVerify;
---
+++ client/src/pages/ResetPassword.jsx
@@ -0,0 +1,9 @@
+
+
+const ResetPassword = () => {
+  return (
+    <div>resetPassword</div>
+  )
+}
+
+export default ResetPassword
